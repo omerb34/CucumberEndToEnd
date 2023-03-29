72c0aa007490649a1f905aa62f02bb05c69227c9
techproed <techproeducation@gmail.com>
2023-03-29 23:18:57 +0300

    29.03.2023

--- src/test/java/base_urls/MedunnaBaseUrl.java
+++ src/test/java/base_urls/MedunnaBaseUrl.java
@@ -4,6 +4,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import static utilities.AuthenticationMedunna.generateToken;
+
 public class MedunnaBaseUrl {
 
     public static RequestSpecification spec;
@@ -12,7 +14,7 @@ public class MedunnaBaseUrl {
 
         spec = new RequestSpecBuilder().
                 setContentType(ContentType.JSON).
-                addHeader("Authorization","Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYXRjaF95dXp1YyIsImF1dGgiOiJST0xFX0FETUlOIiwiZXhwIjoxNjgwMTIxNjU2fQ.Aaie5hSvgn_ALZktQJ25rx_EaY147lYxfUF3cok8Zs3QECuVY8VK8SQVPwQKDymFmRrea_0ojCKXc9s7Ab7_yg").
+                addHeader("Authorization","Bearer "+generateToken()).
                 setBaseUri("https://medunna.com").build();
 
     }
